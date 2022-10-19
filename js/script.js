ed0376c0c52a6aafbb33debbeef0029a795be526
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2022-10-19 16:42:35 +0000

    Finished the whole project

--- js/script.js
+++ js/script.js
@@ -2,6 +2,15 @@
  * Created on: Oct 2022
  * This file contains the JavaScript for index.html
  */
-function myButtonClicked() {
-  document.getElementById("address").innerHTML
+/* FUnction */
+function calculatePay() {
+/* Input */
+  const hoursWorked = parseInt(document.getElementById("hoursWorked").value)
+  const wage = parseInt(document.getElementById("hourlyRate").value)
+  const TAX_RATE = 0.18
+  const pay = hoursWorked * wage * (1 - TAX_RATE)
+  const taxes = hoursWorked * wage * TAX_RATE
+/* Output */
+  document.getElementById("pay").innerHTML = `Your pay will be: $ ${pay.toFixed(2)}`
+  document.getElementById("governmentPortion").innerHTML = 'The government will take: $' + taxes.toFixed(2)
 }
