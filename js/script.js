cc9b9099f30498074666011b899fa08babfb128c
Alex-Nelson-1 <113055690+Alex-Nelson-1@users.noreply.github.com>
2022-10-19 12:08:19 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* Created by: Alex.Nelson
+ * Created on: Oct 2022
+ * This file contains the JavaScript for index.html
+ */
+function myButtonClicked() {
+  document.getElementById("address").innerHTML
+}
